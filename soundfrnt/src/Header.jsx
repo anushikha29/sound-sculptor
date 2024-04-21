7990f66f1594787d373e02820afe4e90b11dfb3d
Adnannn <adnanrashid230@gmail.com>
2024-04-22 03:04:11 +0530

    did_too_much_stuff

---
+++ soundfrnt/src/Header.jsx
@@ -0,0 +1,44 @@
+import './index.css';
+import HeadLogo from './Logo/Logo';
+function Header(){
+    return(
+        <>
+        {/* <!-- Main floating header --> */}
+        <header className="floatingheader" >
+          <div className="box">
+            <nav className="navbar">
+              <ul>
+                <li className="logo"><HeadLogo /></li>
+                <li className="name"><a href="index.html"> Sound Sculptor</a></li>
+                <li><a href className="btm">Home</a></li>
+                <li><a href ="About.html" className="btm">About</a></li>
+              </ul>
+              <ul className="bars">    
+                <li><a href="Sign-in.html" className="btm">Create your account </a></li>                    <li><a href="Sign-in.html" className="btm">Sign in</a></li>
+              </ul>
+            </nav>
+          </div>
+      </header>
+    {/* <!-- Placeholderheader that stays in place of the main header so that about doenst collide into header --> */}
+    <header className="placeholderheader">
+        <div className="box"> 
+            <nav className="navbar1">
+                <ul>
+                    <li className="logo">
+                        <HeadLogo />
+                    </li>
+                    <li className="name"><a href="index.html"> Sound Sculptor</a></li>
+                    <li><a href className="btm">Home</a></li>
+                    <li><a href className="btm">About</a></li>
+                </ul>
+                <ul className="bars">    
+                    <li><a href="Sign-in.html" className="btm">Create your account </a></li>
+                    <li><a href="Sign-in.html" className="btm">Sign in</a></li>
+                </ul>
+            </nav>
+        </div>
+        </header>
+        </>
+    );
+}
+export default Header
---
+++ soundfrnt/src/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import '../index.css'
+function HeadLogo(){
+    return(
+        <div className = "logo">
+            <span className = "musicwave"></span>
+            <span className = "musicwave"></span>
+            <span className = "musicwave"></span>
+            <span className = "musicwave"></span>
+            <span className = "musicwave"></span>
+            <span className = "musicwave"></span>
+            <span className = "musicwave"></span>
+        </div>
+    )
+}
+export default HeadLogo
